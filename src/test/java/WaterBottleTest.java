a9928675118d1eb78827ae4743a5a52030c9c090
belus90 <belus90@gmail.com>
2022-05-09 16:24:10 +0100

    Water bottle homework done

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle bottle;
+    @Before
+    public void before(){
+        bottle = new WaterBottle(100);
+    }
+    @Test
+    public void hasVolume(){
+        assertEquals(100,bottle.getVolume());
+    }
+    @Test
+    public void hasDrink(){
+        bottle.drink();
+        assertEquals(90,bottle.getVolume());
+    }
+    @Test
+    public void hasEmptyBottle(){
+        bottle.emptyBottle();
+        assertEquals(0, bottle.getVolume());
+    }
+    @Test
+    public void hasTheBottleFull(){
+        bottle.setVolume(100);
+        assertEquals(100, bottle.getVolume());
+    }
+}
